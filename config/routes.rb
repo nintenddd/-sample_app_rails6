997f21ef9dd74c2b81cef2c99942c54aace6c265
Bowei Chen <a88115470@gmail.com>
2019-05-16 10:59:12 +0800

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+  get 'sessions/new'
+  scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
     root 'static_pages#home'
     get  '/help',    to: 'static_pages#help'
     get  '/about',   to: 'static_pages#about'
     get  '/contact', to: 'static_pages#contact'
     get  '/signup',  to: 'users#new'
+    get    '/login',   to: 'sessions#new'
+    post   '/login',   to: 'sessions#create'
+    delete '/logout',  to: 'sessions#destroy'
     resources :users
   end
 end
