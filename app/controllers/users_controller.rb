2d344fcbf5033e015d2f812bb65c812c295dc8e3
Bowei Chen <a88115470@gmail.com>
2019-05-20 16:15:04 +0800

    try update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update(user_params)
+    if @user.update(params[:id],user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
     else
