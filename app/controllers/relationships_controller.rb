31b31f52a83f95b6fffae1703b02082476b046c9
Bowei Chen <a88115470@gmail.com>
2019-05-31 09:11:38 +0800

    add @

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,7 +4,7 @@ class RelationshipsController < ApplicationController
 
   def create
     user = User.find(params[:followed_id])
-    current_user.follow(user)
+    current_user.follow(@user)
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
@@ -13,7 +13,7 @@ class RelationshipsController < ApplicationController
 
   def destroy
     user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
+    current_user.unfollow(@user)
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
